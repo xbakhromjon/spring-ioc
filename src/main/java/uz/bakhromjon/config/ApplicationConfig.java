ada8960fcd4f531c7960c488a1e395288b2817db
bakhromjon <khasanboyevbakhromjon8@gmail.com>
2022-02-13 10:07:30 +0500

    auto

--- src/main/java/uz/bakhromjon/config/ApplicationConfig.java
+++ src/main/java/uz/bakhromjon/config/ApplicationConfig.java
@@ -1,20 +1,13 @@
 package uz.bakhromjon.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import uz.bakhromjon.repository.UserRepository;
 import uz.bakhromjon.service.UserService;
 
 @Configuration
+@ComponentScan(basePackages = "uz.bakhromjon")
 public class ApplicationConfig {
 
-    @Bean
-    public UserRepository userRepository() {
-        return new UserRepository();
-    }
-
-    @Bean
-    public UserService userService() {
-        return new UserService(userRepository());
-    }
 }
--- src/main/java/uz/bakhromjon/repository/UserRepository.java
+++ src/main/java/uz/bakhromjon/repository/UserRepository.java
@@ -1,6 +1,10 @@
 package uz.bakhromjon.repository;
 
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserRepository {
     // TODO: 2/13/2022 some functions
 }
