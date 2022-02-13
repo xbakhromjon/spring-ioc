31a44c6d2a43b45fc89a9507bce3370609e02db2
bakhromjon <khasanboyevbakhromjon8@gmail.com>
2022-02-13 09:50:42 +0500

    xml-annotation based

--- src/main/java/uz/bakhromjon/repository/UserRepository.java
+++ src/main/java/uz/bakhromjon/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package uz.bakhromjon.repository;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Component
 public class UserRepository {
     // TODO: 2/13/2022 some functions
 }
--- src/main/java/uz/bakhromjon/service/UserService.java
+++ src/main/java/uz/bakhromjon/service/UserService.java
@@ -2,8 +2,10 @@ package uz.bakhromjon.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import uz.bakhromjon.repository.UserRepository;
 
+@Component
 public class UserService extends AbstractService<UserRepository>{
     @Autowired
     public UserService(UserRepository repository) {
